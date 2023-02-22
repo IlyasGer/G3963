7dfd7fd1b78aba0978a9dca73d4b29cbe4de1f53
Ilyas_Gereihanov <gereihanov_ilyas@mail.ru>
2023-02-22 20:43:53 +0300

    add solution for task 19, 21, 23

---
+++ Sem3Task21/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+ void PrintData(string msg, double val)
+ {
+    Console.WriteLine(msg+val);
+ }
+
+ double CalClen(int x1, int x2, int y1, int y2, int z1, int z2)
+ {
+    return Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
+ }
+ int x1 = ReadData("Введите координату х точки А: ");
+ int x2 = ReadData("Введите координату х точки А: ");
+ int y1 = ReadData("Введите координату y точки В: ");
+ int y2 = ReadData("Введите координату y точки В: ");
+ int z1 = ReadData("Введите координату y точки C: ");
+ int z2 = ReadData("Введите координату y точки C: ");
+
+ double res = CalClen(x1, x2, y1, y2, z1, z2);
+
+ PrintData("Расстояние между точками А и В и C: ", res);
---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+int ReadData(string mgs)
+{
+    Console.WriteLine(mgs);
+    return Convert.ToInt32(Console.ReadLine() ?? "0");
+}
+
+string LineBuilder(int end, int pow)
+{
+    string res = String.Empty;
+
+    for(int i = 1; i<=end; i++)
+    {
+        res = res + Math.Pow(i, pow+1)+ "\t";
+    }
+    return res;
+}
+
+int N = ReadData("Введите конечное число: ");
+Console.WriteLine(LineBuilder(N,0));
+Console.WriteLine(LineBuilder(N,2));
