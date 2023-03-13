da0e635ead03d3428d7dc43bc489317be756d8fe
Ilyas_Gereihanov <gereihanov_ilyas@mail.ru>
2023-03-13 21:11:08 +0300

    add solution for task 64, 66, 68

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Задайте значение N. Напишите программу, 
+//  которая выведет все натуральные числа в промежутке от 1 до N. Выполнить с помощью рекурсии.
+
+// N = 5 -> "1, 2, 3, 3, 4"
+// N = 8 -> "1, 2, 3, 4, 5, 6, 7, 8"
+
+
+int n = InputNumbers("Введите n: ");
+int count = 2;
+PrintNumber(n, count);
+Console.Write(1);
+
+void PrintNumber(int n, int count)
+{
+  if (count > n) return;
+  PrintNumber(n, count + 1);
+  Console.Write(count + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
